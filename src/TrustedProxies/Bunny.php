ff9d829044382565ba6741a4b21e9a2b71c5ac10
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2025-12-01 03:34:44 +0000

    wip

--- src/TrustedProxies/Bunny.php
+++ src/TrustedProxies/Bunny.php
@@ -3,22 +3,29 @@
 namespace HeyJorgeDev\TrustedProxies\TrustedProxies;
 
 use HeyJorgeDev\TrustedProxies\Concerns\TrustedProxiesProvider;
-use Illuminate\Http\Client\Batch;
-use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 class Bunny implements TrustedProxiesProvider
 {
     public function get(): array
     {
-        $responses = Http::batch(fn (Batch $batch) => [
-            $batch->asJson()->get('https://bunnycdn.com/api/system/edgeserverlist'),
-            $batch->asJson()->get('https://bunnycdn.com/api/system/edgeserverlist/IPv6'),
-        ])->send();
+        return [
+            ...$this->getIPv4(),
+            ...$this->getIPv6(),
+        ];
+    }
 
-        return collect($responses)
-            ->map(fn (Response $response) => $response->json())
-            ->flatten()
-            ->all();
+    private function getIPv4(): array
+    {
+        return Http::asJson()
+            ->get('https://bunnycdn.com/api/system/edgeserverlist')
+            ->json();
+    }
+
+    private function getIPv6(): array
+    {
+        return Http::asJson()
+            ->get('https://bunnycdn.com/api/system/edgeserverlist/IPv6')
+            ->json();
     }
 }
